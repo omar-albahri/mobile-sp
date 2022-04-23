6ae8e314401e055f86ca22ebf9978b925a265e75
omar-bahri <omarbahriit@gmail.com>
2022-04-23 19:56:20 +0300

    style

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -21,7 +21,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.rtlcss.com/bootstrap/v4.5.3/js/bootstrap.bundle.min.js" integrity="sha384-40ix5a3dj6/qaC7tfz0Yr+p9fqWLzzAXiwxVLt9dw7UjQzGYw6rWRhFAnRapuQyK" crossorigin="anonymous"></script>
     -->
-    <link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
+    {{--<link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">--}}
     <script src="{{asset('frontend/lib/jquery.min.js') }}"></script>
     <link rel="stylesheet" href="{{asset('frontend/lib/bootstrap.min.css') }}">
     <script src="{{asset('frontend/lib/bootstrap.min.js') }}"></script>
